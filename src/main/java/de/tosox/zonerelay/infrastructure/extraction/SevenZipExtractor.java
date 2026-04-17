f52a08f143daf1b2d32fb0c75b82c5d0f6d1a994
Tosox <57193602+Tosox@users.noreply.github.com>
2026-04-17 18:35:10 +0200

    fix: Prevent possible OS process leak

--- src/main/java/de/tosox/zonerelay/infrastructure/extraction/SevenZipExtractor.java
+++ src/main/java/de/tosox/zonerelay/infrastructure/extraction/SevenZipExtractor.java
@@ -28,22 +28,25 @@ public class SevenZipExtractor implements ArchiveExtractor {
 	public void extract(File archive, Path destination) throws Exception {
 		logger.info("Extracting %s to %s", archive.getPath(), destination);
 
-		ProcessBuilder builder = new ProcessBuilder(
-				sevenZipPath, "-bso0", "x", archive.getPath(), "-o" + destination.toString(), "-y"
-		);
-		builder.redirectErrorStream(true);
-		Process process = builder.start();
+		Process extractProcess = new ProcessBuilder(
+				sevenZipPath, "-bso0", "x", archive.getPath(), "-o" + destination.toString(), "-y")
+				.redirectErrorStream(true)
+				.start();
 
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				logger.info(line);
+		try {
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(extractProcess.getInputStream()))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					logger.info(line);
+				}
 			}
-		}
-
-		int exitCode = process.waitFor();
-		if (exitCode != 0) {
-			throw new IOException("Extraction process failed with exit code " + exitCode);
+			int exitCode = extractProcess.waitFor();
+			if (exitCode != 0) {
+				throw new IOException("Extraction process failed with exit code " + exitCode);
+			}
+		} catch (Exception e) {
+			extractProcess.destroyForcibly();
+			throw e;
 		}
 	}
 }
