e815e0c27a49abc62b2bdf1e613935b43385453d
Tosox <57193602+Tosox@users.noreply.github.com>
2026-05-02 00:24:20 +0200

    feat: Update User-Agent browser header (#76)

--- src/main/java/de/tosox/zonerelay/infrastructure/download/HttpClient.java
+++ src/main/java/de/tosox/zonerelay/infrastructure/download/HttpClient.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 
 @Singleton
 public class HttpClient {
-	private static final String USER_AGENT = "Mozilla/5.0 (X11; Linux i686; rv:57.0) Gecko/20100101 Firefox/57.0";
+	private static final String USER_AGENT =
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
 
 	private final OkHttpClient client;
 
